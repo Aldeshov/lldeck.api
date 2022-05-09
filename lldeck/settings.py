1d3cec45894c6161188e8141a5b6224ba7f307fd
Aldeshov <azata1919@gmail.com>
2022-05-09 16:08:26 +0600

    Limit pagination settings to 10

--- lldeck/settings.py
+++ lldeck/settings.py
@@ -187,7 +187,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
 }
 
 LOGGING = {
